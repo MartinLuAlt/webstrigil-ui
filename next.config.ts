ec5fe5851b078a22cfc50b43f5113a26206dd392
Momopey <martinylu@berkeley.edu>
2025-04-17 10:54:23 -0700

    Git action deploy static page to cloudfront

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 👈 enables static export
+  reactStrictMode: true, // optional but good for dev
+  trailingSlash: true, // 👈 required for S3 to resolve folders correctly
 };
 
 export default nextConfig;
